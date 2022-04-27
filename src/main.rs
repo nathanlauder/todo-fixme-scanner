9b34a54f45cd7f1e2a5d58ddeabe4cacd6bf49d5
nathanlauder <laudern42@gmail.com>
2022-04-27 11:11:06 -0500

    ignore specified hidden directories

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#![allow(non_snake_case)]
+
+use ignore::{WalkBuilder, DirEntry};
+mod arguments;
+use arguments::getArguments;
+
+fn is_ignored_dir(entry: &DirEntry) -> bool {
+  if entry.metadata().unwrap().file_type().is_dir() &&
+   getArguments().ignoreDirs.len() >= 1 {
+    let splitPath: Vec<&str> = entry.path().to_str().unwrap().split("/").collect();
+    let dir: String = splitPath[splitPath.len()-1].to_string();
+    if getArguments().ignoreDirs.contains(&dir) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return false;
+}
+
 fn main() {
-    println!("Hello, world!");
+  let args = getArguments();
+  
+  for result in WalkBuilder::new(args.path).filter_entry(|path| !is_ignored_dir(path)).hidden(false).build() {
+    println!("{:?}\n", result.unwrap().path().display());
+  }
 }
