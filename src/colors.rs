408d8b8546840395b258e6f68d8b2f68ea7c289c
Nathan Lauder <laudern42@gmail.com>
2023-04-29 18:57:44 -0500

    add green print

--- src/colors.rs
+++ src/colors.rs
@@ -1,18 +1,22 @@
 extern crate colored;
 use colored::*;
 
-pub fn printer() {
-  println!(
-      "{}, {}, {}, {}, {}, {}, and some normal text.",
-      format!("Bold").bold(),
-      format!("Red").red(),
-      format!("Yellow").yellow(),
-      format!("Green Strikethrough").green().strikethrough(),
-      format!("Blue Underline").blue().underline(),
-      format!("Purple Italics").purple().italic()
-  );
-}
+// pub fn printer() {
+//   println!(
+//       "{}, {}, {}, {}, {}, {}, and some normal text.",
+//       format!("Bold").bold(),
+//       format!("Red").red(),
+//       format!("Yellow").yellow(),
+//       format!("Green Strikethrough").green().strikethrough(),
+//       format!("Blue Underline").blue().underline(),
+//       format!("Purple Italics").purple().italic()
+//   );
+// }
 
 pub fn printRed(msg: String) {
-  println!("{}", String::from(msg).red().bold());
+  println!("{}", msg.red().bold());
+}
+
+pub fn printGreen(msg: String) {
+  println!("{}", msg.green().bold());
 }
